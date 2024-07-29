89b45d80fb701a550e4a93566ab70974886b6091
Andrew Chacon <chaconandrew226@gmail.com>
2024-07-29 15:02:55 -0400

    implemented drag and drop with google images

--- recipe-chef/src/components/Title.jsx
+++ recipe-chef/src/components/Title.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 
 function Title() {
     const [title, setTitle] = useState("NAME OF FOOD")
+    const [image, setImage] = useState("")
 
   return (
     <div style={{display: 'flex', flexDirection: 'row'}}>
-        <input type='text' style={{border: 'none', width: '45vw', height: '10vh', fontSize: '24px'}} value={title} onChange={e => setTitle(e.target.value)} />
-        <div className="gcse-search" data-enableImageSearch="true" data-defaultToImageSearch="true" data-disableWebSearch={true}></div>
+        <input type='text' style={{border: 'none', width: '20vw', height: '10vh', fontSize: '24px'}} value={title} onChange={e => setTitle(e.target.value)} />
+        <div onDragOver={e => e.preventDefault()} onDrop={e => setImage(e.dataTransfer.mozSourceNode.src)} style={{width: '20vw', height: '10vh', backgroundColor: 'gray'}}><img src={image} alt='food' /></div>
     </div>
   )
 }
