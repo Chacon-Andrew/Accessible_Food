681e04d06c2e96d6824d9551c6ade581348bcd80
Andrew Chacon <chaconandrew226@gmail.com>
2024-08-04 13:13:42 -0400

    added in names for tools and ingredients

--- recipe-chef/src/components/Ingredients.jsx
+++ recipe-chef/src/components/Ingredients.jsx
@@ -9,18 +9,27 @@ function Ingredients() {
 
     const handleImageDrop = (e, i) => {
         const newImages = [...images]
-        newImages[i] = e
+        newImages[i].img = e
         setImages(newImages)
     }
 
     const handleImageAdd = () => {
         const newImages = [...images]
-        newImages.push("")
+        newImages.push({
+            img: "",
+            name: ""
+        })
         setImages(newImages)
     }
 
     const handleImageRemove = (i) => {
         const newImages = images.filter((e, index) => index !== i)
+        setImages(newImages)
+    }
+
+    const handleNameAdd = (e, i) => {
+        const newImages = [...images]
+        newImages[i].name = e
         setImages(newImages)
     }
 
@@ -30,11 +39,12 @@ function Ingredients() {
             {images.map((element, index) => {
                 return(
                     <Grid item xs={12} sm={3} key={index}>
-                        <div onDragOver={e => e.preventDefault()} onDrop={e => handleImageDrop(e.dataTransfer.mozSourceNode.src, index)} style={{padding: '1em', backgroundColor: 'gray', display: 'flex', flexDirection: 'column'}}>
+                        <div onDragOver={e => e.preventDefault()} onDrop={e => handleImageDrop(e.dataTransfer.mozSourceNode.src, index)} style={{padding: '1em', backgroundColor: '#D9D9D9', display: 'flex', flexDirection: 'column', gap: '1em'}}>
                             <div style={{position: 'relative'}}>
                                 <IconButton onClick={() => handleImageRemove(index)}><CloseIcon /></IconButton>
                             </div>
-                            <img src={element} alt='Drag in Ingredient image' />
+                            <input type='text' value={element.name} onChange={e => handleNameAdd(e.target.value, index)} style={{border: 'none', fontSize: '24px', textAlign: 'center', backgroundColor: '#D9D9D9'}} placeholder='Enter Text Here'/>
+                            <img src={element.img} alt='Drag in Ingredient image' />
                         </div>
                     </Grid>
                 )
